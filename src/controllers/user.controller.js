8513d9304718739398cf8c5844511fc72924e3c4
Anil Kambar <anil.aham11@gmail.com>
2026-09-15 12:58:16 +0530

    minor bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -145,7 +145,7 @@ const loginUser = asyncHandler(async (req, res) => {
       new ApiResponse(
         200,
         { user: loggedInUser, accessToken, refreshToken },
-        "User logged in successfully"
+        `${user.username} logged in successfully`
       )
     );
 });
